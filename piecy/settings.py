9dc563d56dafa7a0b83fc91ecd91f0e74ff6de2b
Julinium <julinium@gmail.com>
2025-07-21 14:42:43 +0100

    accounts pages styling

--- piecy/settings.py
+++ piecy/settings.py
@@ -76,13 +76,14 @@ ROOT_URLCONF = 'piecy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'base.context_processors.available_languages',
             ],
         },
     },
